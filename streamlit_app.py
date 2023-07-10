c756805d35836ffaea6cc74de6173b9741835d27
Sai Ram Manikanta <sairamgithub@gmail.com>
2023-07-10 08:09:17 +0530

    Update streamlit_app.py
    instead of fruits, given variable name as fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruits_list = my_fruits_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index), ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 
 # display table on the page
