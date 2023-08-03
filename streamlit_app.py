8244f20307cd56cd1e25da7e663eb224beb39487
Sai Ram Manikanta <sairamgithub@gmail.com>
2023-08-03 07:58:43 +0530

    Update streamlit_app.py
    created variable to specify fruit name in the API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,12 @@ streamlit.dataframe(fruits_to_show)
 
 # header
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input(''What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # display fruityvice api response 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # normalize the JSON response
