35e3b6fe1fa36b2c2717b68e03b9b7aa455b8c66
Sai Ram Manikanta <sairamgithub@gmail.com>
2023-08-03 07:51:46 +0530

    Update streamlit_app.py
    spelling corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 # normalize the JSON response
-fruityvice_normalized = pandas.json_normalize(fruiyvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 #Output the data on the screen as table 
 streamlit.dataframe(fruityvice_normalized)
