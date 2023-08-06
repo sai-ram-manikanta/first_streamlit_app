f64dc0afc7f63e91dc21448f1f744c56cc763a03
Sai Ram Manikanta <sairamgithub@gmail.com>
2023-08-06 20:15:35 +0530

    Update streamlit_app.py
    corrected function name

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ streamlit.write('The user entered ', fruit_choice)
 # import snowflake.connector
 streamlit.header("The fruit load list contains:")
 # SF related functions
-def get_fruits_load_list():
+def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
